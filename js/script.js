9eb680872087c95bba71544369dc8fab94d90415
BennehR <5487055+BennehR@users.noreply.github.com>
2024-11-27 23:26:56 +0000

    Basic prototype Buttons which are conditional Only site buttons work currently

--- js/script.js
+++ js/script.js
@@ -1,8 +1,46 @@
-function showText() {
-    const testButton = document.getElementById("textField");
-    if (testButton.style.display === "none") {
-        testButton.style.display = "block";
-    } else {
-        testButton.style.display = "none";
-    }
+const nrfButton = document.getElementById("buttonNRF");
+const tcrcButton = document.getElementById("buttonTCRC");
+const tpphButton = document.getElementById("buttonTPPH");
+
+const siteNRF = document.getElementById("siteNRF");
+const siteTPPH = document.getElementById("siteTPPH");
+const siteTCRC = document.getElementById("siteTCRC");
+
+function resetSelection() {
+    siteNRF.style.display = "none";
+    nrfButton.classList.remove("selected-button");
+    siteTPPH.style.display = "none";
+    tpphButton.classList.remove("selected-button");
+    siteTCRC.style.display = "none";
+    tcrcButton.classList.remove("selected-button");
+}
+
+function selectNRF() {
+    resetSelection();
+    siteNRF.style.display = "block";
+    nrfButton.classList.add("selected-button");
+}
+
+function selectTCRC() {
+    resetSelection();
+    siteTCRC.style.display = "block";
+    tcrcButton.classList.add("selected-button");
+}
+
+function selectTPPH() {
+    resetSelection();
+    siteTPPH.style.display = "block";
+    tpphButton.classList.add("selected-button");
+}
+
+function selectTPPH_R1 () {
+
+}
+
+function selectTPPH_R2 () {
+    
+}
+
+function selectTPPH_R3 () {
+    
 }

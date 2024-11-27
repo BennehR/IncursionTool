0e31357439a8c41fdf07984dbe9fbe466a4ee2e3
BennehR <5487055+BennehR@users.noreply.github.com>
2024-11-27 00:20:34 +0000

    baby steps

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showText() {
+    const testButton = document.getElementById("textField");
+    if (testButton.style.display === "none") {
+        testButton.style.display = "block";
+    } else {
+        testButton.style.display = "none";
+    }
+}
